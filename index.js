96fb8dec9891f8334e580acaa79809cb5bcb77d0
Tauhid hossen <tauhidhossen04@gmail.com>
2024-09-09 00:18:43 +0600

    create some Api for cart collection

--- index.js
+++ index.js
@@ -120,8 +120,39 @@ async function run() {
       res.send(result);
     })
 
-
+    //cart Routes !.....
+    app.post('/add-to-cart', async(req,res)=>{
+      const newCartItem = req.body;
+      const result = await cartCollection.insertOne(newCartItem);
+      res.send(result);
+    })
     
+    //get cart items by id
+    app.get('/cart-item/:id', async(req,res)=>{
+      const id = req.params.id;
+      const email =req.body.email;
+      const query ={
+        classId : id,
+        userMail : email
+      }
+      const projection = {classId:1};
+      const result = await cartCollection.findOne(query, {projection: projection});
+      res.send(result);
+    })
+
+    //cart info by user email
+    app.get('/cart/:email', async(req,res)=>{
+      const email = req.params.email;
+      const query = {userMail: email};
+      const projection = {classId: 1};
+      const carts =await cartCollection.find(query,{projection: projection});
+      const classIds = carts.map((cart) => new ObjectId(cart.classId));
+      const query2 = {_id: {$in: classIds}};
+      const result = await classesCollection.find(query2).toArray();
+      res.send(result);
+    });
+  
+
 
 
     // Send a ping to confirm a successful connection
